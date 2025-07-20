bbac9f02deffc57f3c0467879226dac8779f400e
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2025-07-19 20:41:59 -0400

    Create idealVariableTypes.java

---
+++ idealVariableTypes.java
@@ -0,0 +1,9 @@
+class idealVariableTypes {
+    public static void main(String[] args) {
+        double money; //The amount of money in someone's bank account will have decimals, and it may be very large  
+        int sumOfNumberSeries; //They're all natural numbers/integers, so no need for the variable to store decimals
+        String robotName; //Strings can handle words 
+        double sphereVolume; //The volume of a sphere can have decimals, and it might be very large
+        long neuronCount; //The number of neurons in someone's brain is a massive number but it's not a decimal as you can't have half a neuron
+    }
+}
