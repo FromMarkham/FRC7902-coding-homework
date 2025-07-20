09c6e42ce9878498f696793d413e76efce9b0338
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2025-07-19 21:11:42 -0400

    Update LoopExample.java
    Adjusted the comments

--- LoopExample.java
+++ LoopExample.java
@@ -1,12 +1,8 @@
 /**
- * This LoopExample class uses a while loop. 
- * The loop starts from a certain number and increments it by some amount
- * until it reaches or exceeds a certain point.
  * 
- * It uses an integer that starts at 5 and increases by 3 on each loop iteration
- * until it is no longer less than 105.
- * 
- * 
+ * This program starts from 5 and increments it by 3
+ * until it exceeds 105 using a while loop.
+ *
  * By: Bob Wang 
  */
 public class LoopExample {
